223d7918c9c06816ae4cce659676326fbd6caf84
cristian23tbtb <cristian.valenzuela1901@alumnos.ubiobio.cl>
2023-07-23 00:09:45 -0400

    back

--- app.js
+++ app.js
@@ -21,6 +21,57 @@ const mc = mysql.createConnection({
 mc.connect();
 
 //Rutas
+
+//Agregar productos
+app.post('/agregarproducto', function (req, res) {
+    let datosProducto = {
+        base_id: req.body.base_id,
+        diseno_id: req.body.diseno_id,
+        precio: parseInt(req.body.precio)
+    };
+
+    if (mc) {
+        mc.query("INSERT INTO producto SET ?", datosProducto, function (error, result) {
+            if (error) {
+                res.status(500).json({"Mensaje": "Error al insertar el producto"});
+            } else {
+                res.status(201).json({"Mensaje": "Producto insertado correctamente"});
+            }
+        });
+    }
+});
+
+//Borrar producto
+app.delete('/borrarp/:id', function (req, res){
+    let id = req.params.id;
+    if(mc){
+        console.log(id);
+        mc.query("DELETE FROM producto WHERE id_producto = ?", id, function (error, result){
+            if(error){
+                return res.status(500).json({"Mensaje": "Error"});
+            }
+            else{
+                return res.status(200).json({"Mensaje": "Registro con id = " + id + " Borrado"});
+            }
+        });
+    }
+});
+
+//Actualizar producto
+app.put('/actualizarp/:id', (req, res) =>{
+    let id = req.params.id;
+    let producto = req.body;
+    console.log(id);
+    console.log(producto);
+    if (!id || !producto){
+        return res.status(400).send({error: producto, message: 'Debe proveer un id y los datos de un producto'});
+    }
+    mc.query("UPDATE producto SET ? WHERE id_producto = ?", [producto, id], function (error, results, fields){
+        if (error) throw error;
+        return res.status(200).json({"Mensaje": "Registro con id = " + id + " ha sido actualizado"});
+    });
+});
+
 //Recuperar todos los productos
 app.get('/producto', function (req, res) {
     mc.query('SELECT * FROM producto', function(error, results, fields){
@@ -29,6 +80,86 @@ app.get('/producto', function (req, res) {
             error: false,
             data: results,
             message: 'Lista de productos.'
+        });
+    });
+})
+
+app.get('/',(req,res,next) => {
+    res.status(200).json({
+        ok:true,
+        mensaje: 'Peticion reañizada correctamente'
+    })
+});
+
+//Existe producto
+app.get('/existeproducto/:id', (req, res, next) => {
+    let id = req.params.id;
+    console.log('id:' + id);
+    mc.query("SELECT * FROM producto WHERE id_producto = ?", id, function (err, result, fields) {
+        return res.send({
+            error: false,
+            data: result,
+            message: 'Producto existe'
+        });
+    });
+});
+
+//Agregar diseños
+app.post('/agregardiseno', function (req, res) {
+    let datosDiseno = {
+        diseno: req.body.diseno
+    };
+
+    if (mc) {
+        mc.query("INSERT INTO diseno SET ?", datosDiseno, function (error, result) {
+            if (error) {
+                res.status(500).json({"Mensaje": "Error al insertar el diseño"});
+            } else {
+                res.status(201).json({"Mensaje": "Diseño insertado correctamente"});
+            }
+        });
+    }
+});
+
+//Borrar producto
+app.delete('/borrard/:id', function (req, res){
+    let id = req.params.id;
+    if(mc){
+        console.log(id);
+        mc.query("DELETE FROM diseno WHERE id_diseno = ?", id, function (error, result){
+            if(error){
+                return res.status(500).json({"Mensaje": "Error"});
+            }
+            else{
+                return res.status(200).json({"Mensaje": "Registro con id = " + id + " Borrado"});
+            }
+        });
+    }
+});
+
+//Actualizar diseno
+app.put('/actualizard/:id', (req, res) =>{
+    let id = req.params.id;
+    let diseno = req.body;
+    console.log(id);
+    console.log(diseno);
+    if (!id || !diseno){
+        return res.status(400).send({error: diseno, message: 'Debe proveer un id y los datos de un diseno'});
+    }
+    mc.query("UPDATE diseno SET ? WHERE id_diseno = ?", [diseno, id], function (error, results, fields){
+        if (error) throw error;
+        return res.status(200).json({"Mensaje": "Registro con id = " + id + " ha sido actualizado"});
+    });
+});
+
+//Recuperar todos los diseños
+app.get('/diseno', function (req, res) {
+    mc.query('SELECT * FROM diseno', function(error, results, fields){
+        if(error) throw error;
+        return res.send({
+            error: false,
+            data: results,
+            message: 'Lista de diseños.'
         });
     });
 })
